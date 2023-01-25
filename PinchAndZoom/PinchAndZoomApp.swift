c24e7828ad4e82161ba0250d4081c91af15d2efe
rafroes1 <rafafroes@outlook.com>
2023-01-25 11:31:10 -0300

    Initial Commit

---
+++ PinchAndZoom/PinchAndZoomApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchAndZoomApp.swift
+//  PinchAndZoom
+//
+//  Created by Rafael Carvalho on 25/01/23.
+//
+
+import SwiftUI
+
+@main
+struct PinchAndZoomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
